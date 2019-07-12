924c3a3333d9c87e4c351f35ef301cb3655bcd67
johan campion <johan-externe.campion@edf.fr>
2019-07-12 13:26:36 +0200

    ajout fixture pour les product

--- src/DataFixtures/ProductFixture.php
+++ src/DataFixtures/ProductFixture.php
@@ -2,7 +2,9 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Category;
 use App\Entity\Product;
+use App\Entity\Tag;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
@@ -12,14 +14,29 @@ class ProductFixture extends Fixture
     {
         $product = new Product();
         $product->setTitle('Geforce RTX 2080 Ti');
-        //$product->setCategory('GPU');
+        $category = new Category();
+        $category->setTitle('GPU');
+        $product->setCategory($category);
+        $tag = new Tag();
+        $tag->setTitle('hardware');
+        $product->addTag($tag);
         $product->setContent('La toute nouvelle carte graphique NVIDIA est une révolution tant dans le réalisme que dans les performances de vos jeux. Sa puissante architecture NVIDIA Turing™ , ses technologies à couper le souffle et sa mémoire GDDR6 de 11 Go nouvelle génération en font le système GPU ultime en matière de jeux.');
+        $manager->persist($category);
+        $manager->persist($tag);
         $manager->persist($product);
+
 
         $product = new Product();
         $product->setTitle('AMD Ryzen™ Threadripper™');
-        //$product->setCategory('CPU');
+        $category = new Category();
+        $category->setTitle('CPU');
+        $product->setCategory($category);
+        $tag = new Tag();
+        $tag->setTitle('hardware');
+        $product->addTag($tag);
         $product->setContent('Déchaînez la force des processeurs AMD Ryzen™ Threadripper™ de 2e génération offrant 32 cœurs et 64 threads de puissance de traitement brute');
+        $manager->persist($category);
+        $manager->persist($tag);
         $manager->persist($product);
 
 
